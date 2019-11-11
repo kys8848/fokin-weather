3b2152179429cf92ff1f10907db53f9165829e68
YS <kys8848@naver.com>
2019-11-11 22:24:46 +0900

    1.0

--- App.js
+++ App.js
@@ -4,21 +4,32 @@ import { StyleSheet, Text, View} from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>휘섭이가 똥쌀때 바지를 몇번 벗는지 계산하는 앱</Text>
+      <View style={styles.yellowView}>
+        
+      </View>
+      
+      <View style={styles.blueView}>
+      
+      </View>      
+     
      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container: { // 컨테이너는 아빠다 (이게 사라지면 밑에는 작동안함 flex:1로 인해 모든공간을 사용하도록함)
     flex: 1,
-    backgroundColor: 'red',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    justifyContent: 'center'
   },
-  text: {
-    color:"white"
-
+  yellowView: {
+    flex: 1,
+    backgroundColor:"yellow"
   },
+  blueView: {
+    flex: 5,
+    backgroundColor:"blue"
+  }
+  
 });
