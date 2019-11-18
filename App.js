8877e83116f906dc7e754f3beb38d0d1609f1f71
YS <kys8848@naver.com>
2019-11-18 23:23:07 +0900

    1.1

--- App.js
+++ App.js
@@ -1,35 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+
+import Loading from "./Loading";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        
-      </View>
-      
-      <View style={styles.blueView}>
-      
-      </View>      
-     
-     
-    </View>
-  );
+  return <Loading />;
 }
-
-const styles = StyleSheet.create({
-  container: { // 컨테이너는 아빠다 (이게 사라지면 밑에는 작동안함 flex:1로 인해 모든공간을 사용하도록함)
-    flex: 1,
-    //alignItems: 'center',
-    justifyContent: 'center'
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor:"yellow"
-  },
-  blueView: {
-    flex: 5,
-    backgroundColor:"blue"
-  }
-  
-});
